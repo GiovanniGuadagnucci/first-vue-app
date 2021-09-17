93a3c04c959b912ec8bcf9fd1f661d30f2b88524
Giovanni Guadagnucci <giovanniguadag@gmail.com>
2021-09-16 22:32:58 -0300

    Quasar

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,101 @@
-        <template>
-  <div class="container">
-    <div class="cards-wrapper">
-      <ul>
-        <li v-for="pokemon in pokemons" v-bind:key="pokemon.name">
-          <div class="card">
-            <img :src="pokemon.sprite" alt="" />
-            <div class="card-content">
-              <h1>{{ pokemon.name }}</h1>
-              <p>{{ pokemon.type }}</p>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </div>
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Essential Links</q-item-label>
+        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="school" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Docs</q-item-label>
+            <q-item-label caption>quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Github</q-item-label>
+            <q-item-label caption>github.com/quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="chat" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Discord Chat Channel</q-item-label>
+            <q-item-label caption>chat.quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="forum" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Forum</q-item-label>
+            <q-item-label caption>forum.quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
+          <q-item-section avatar>
+            <q-icon name="rss_feed" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Twitter</q-item-label>
+            <q-item-label caption>@quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <HelloWorld />
+    </q-page-container>
+  </q-layout>
 </template>
+
 <script>
-import axios from "axios";
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  data() {
+  name: 'LayoutDefault',
+
+  components: {
+    HelloWorld
+  },
+
+  setup () {
     return {
-      pokemons: [],
-    };
-  },
-
-  created() {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=30&offset=0")
-      .then((response) =>
-        response["data"]["results"].forEach((element) => {
-          axios.get(element["url"]).then((pokemon) =>
-            this.pokemons.push({
-              sprite:
-                pokemon["data"]["sprites"]["other"]["dream_world"][
-                  "front_default"
-                ],
-              name: pokemon["data"]["name"],
-              type: pokemon["data"]["types"][0]["type"]["name"],
-            })
-          );
-        })
-      );
-  },
-};
+      leftDrawerOpen: ref(false)
+    }
+  }
+}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-ul li {
-  list-style-type: none;
-}
-
-.card {
-  border: 1px solid #2c3e50;
-  width: 300px;
-}
-img {
-  height: 30vh;
-  background-color: #2c3e50;
-  border-radius: 5px;
-}
-</style>
